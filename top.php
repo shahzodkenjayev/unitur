f0e00c0d8b1bc6b3f88c0ec2cbc72ec1a055c326
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-19 02:50:21 +0500

    Update top.php

--- top.php
+++ top.php
@@ -7,12 +7,12 @@
 						</div>
 						<div class="menu">
 						<ul id="nav">
-							 <li><a href="index.php#section-1"  onclick="javascript:window.open('index.php#section-1','_self')">                            ASOSIY</a></li>
-							 <li><a href="index.php#section-2"  onclick="javascript:window.open('index.php#section-2','_self')">                           BIZ HAQIMIZDA</a></li>
-							 <li><a href="index.php#section-3"  onclick="javascript:window.open('index.php#section-3','_self')">                             GALEREYA</a></li>
-                        <li><a href="category.php" onclick="javascript:window.open('category.php','_self')"> 										KATEGORIYA</a></li>                             
-							 <li><a href="index.php#section-4"   onclick="javascript:window.open('index.php#section-4','_self')">                            REKLAMA</a></li>
-							 <li><a href="index.php#section-5"   onclick="javascript:window.open('index.php#section-5','_self')">                              KONTAKTLAR</a></li>
+							 <li><a href="index.php#section-1"  onclick="javascript:window.open('index.php#section-1','_self')">ASOSIY</a></li>
+							 <li><a href="index.php#section-2"  onclick="javascript:window.open('index.php#section-2','_self')">BIZ HAQIMIZDA</a></li>
+							 <li><a href="index.php#section-3"  onclick="javascript:window.open('index.php#section-3','_self')">GALEREYA</a></li>
+                        				 <li><a href="category.php" onclick="javascript:window.open('category.php','_self')">KATEGORIYA</a></li>                             
+							 <li><a href="index.php#section-4"   onclick="javascript:window.open('index.php#section-4','_self')">REKLAMA</a></li>
+							 <li><a href="index.php#section-5"   onclick="javascript:window.open('index.php#section-5','_self')">KONTAKTLAR</a></li>
 							 <div class="clearfix"></div>
 						 </ul>
 						 </div>
