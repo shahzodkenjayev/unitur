17267873b3511a6ae5ce4a7c2db128d358d9f27b
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-19 02:17:39 +0500

    Update adduser.php

--- Admin/adduser.php
+++ Admin/adduser.php
@@ -17,6 +17,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+
+
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--js--> 
 <script src="js/jquery.min.js"></script>
@@ -46,17 +49,14 @@ if($_SESSION['loginstatus']=="")
 if(isset($_POST["sbmt"]))
 {
 	$cn=makeconnection();
-$sql = "INSERT INTO users (Username, Pwd, Typeofuser) VALUES ('profi', '" . password_hash('profi117', PASSWORD_DEFAULT) . "', 'Admin')";
-	// Parolni hesh qilish
-	$hashed_password = password_hash($_POST["t2"], PASSWORD_DEFAULT); // Parolni xavfsiz hesh qilib o'zgartirish
-
-	// Foydalanuvchini bazaga kiritish
-	$s="INSERT INTO users (Username, Pwd, Typeofuser) VALUES('" . $_POST["t1"] ."', '" . $hashed_password . "', '" . $_POST["s1"] . "')";
-	mysqli_query($cn, $s);
+	$s="insert into users values('" . $_POST["t1"] ."','" . $_POST["t2"] . "','" . $_POST["s1"] . "')";
+	mysqli_query($cn,$s);
 	mysqli_close($cn);
-	echo "<script>alert('Record Saved');</script>";
+	echo "<script>alert('Record Save');</script>";
 }
 ?>
+
+
 
 <?php include('top.php'); ?>
 <!--/sticky-->
@@ -66,20 +66,30 @@ $sql = "INSERT INTO users (Username, Pwd, Typeofuser) VALUES ('profi', '" . pass
 </div>
 <div class="col-sm-9">
 
+
+
+
 <form method="post">
 <table border="0" width="400px" height="300px" align="center" class="tableshadow">
 <tr><td colspan="2" class="toptd">Add User</td></tr>
-<tr><td class="lefttxt">User Name</td><td><input type="text" name="t1" required pattern="[a-zA-z1 _]{3,50}" title="Please Enter Only Characters and numbers between 3 to 50 for User name" /></td></tr>
-<tr><td class="lefttxt">Password</td><td><input type="password" name="t2" required pattern="[a-zA-z0-9]{1,10}" title="Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
-<tr><td class="lefttxt">Confirm Password</td><td><input type="password" name="t3" required pattern="[a-zA-z0-9]{1,10}" title="Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
+<tr><td class="lefttxt">User Name</td><td><input type="text" name="t1" required pattern="[a-zA-z1 _]{3,50}" title"Please Enter Only Characters and numbers between 3 to 50 for User name" /></td></tr>
+<tr><td class="lefttxt">Password</td><td><input type="password" name="t2" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
+<tr><td class="lefttxt">Confirm Password</td><td><input type="password" name="t3" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
 <tr><td class="lefttxt">Type of User</td><td><select name="s1" required><option value="">Select</option><option value="Admin">Admin</option><option value="General">General</option></select></td></tr>
-<tr><td>&nbsp;</td><td><input type="submit" value="SAVE" name="sbmt" /></td></tr>
+<tr><td>&nbsp;</td><td ><input type="submit" value="SAVE" name="sbmt" /></td></tr>
+
+
+
+
 </table>
 </form>
 
-</div>
+
+
 </div>
 
+
+</div>
 <?php include('bottom.php'); ?>
 </body>
 </html>
