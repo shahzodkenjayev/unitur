62aeaf2b2560ad32b0fc5a3b1c58a65f88509eff
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-19 01:26:13 +0500

    Update category.php

--- category.php
+++ category.php
@@ -1,59 +1,80 @@
-<!DOCTYPE html>
-<html lang="uz">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UNITUR | Bosh sahifa</title>
-    <link href="stylecss.css" rel="stylesheet" type="text/css"/>
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-    <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script>new WOW().init();</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>My-tour bootstrap Design website | Home :: w3layouts</title>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href="stylecss.css" rel='stylesheet' type='text/css'/>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
 </head>
+
 <body>
-    <?php include('function.php'); ?>
-    <?php include('top.php'); ?>
-    <?php include('slider.php'); ?>
+<?php include('function.php'); ?>
+<?php include('top.php'); ?>
+<!--/sticky-->
+<?php include('slider.php'); ?>
+<div style="height:50px"></div>
+<div style="width:1000px; margin:auto" >
 
-    <div style="height:50px"></div>
-    <div style="width:1000px; margin:auto; display:flex;">
+<div style="width:200px; float:left">
 
-        <!-- Kategoriya jadvali -->
-        <div style="width:30%; padding:10px;">
-            <h3 style="font-family:Lucida Calligraphy; font-size:20px; color:#09F;">Kategoriyalar</h3>
-            <?php
-function makeconnection() {
-    $host = 'localhost';
-    $username = 'root';
-    $password = 'profi117'; // Bu yerda yangi parolni yozing
-    $database = 'travel'; // Ma'lumotlar bazasining nomi
+<table cellpadding="0" cellspacing="0" width="1000px">
+<tr><td style="font-family:Lucida Calligraphy; font-size:20px; color:#09F"><b>Category</b></td></tr>
+<?php
 
-    $cn = mysqli_connect($host, $username, $password, $database);
 
-    if (!$cn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+$s="select * from category";
+$result=mysqli_query($cn,$s);
+$r=mysqli_num_rows($result);
+//echo $r;
 
-    return $cn;
+while($data=mysqli_fetch_array($result))
+{
+	
+		echo "<tr><td style=' padding:5px;'><b><a href='subcat.php?catid=$data[0]'>$data[1]</a></b></td></tr>";
+
 }
+mysqli_close($cn);
 ?>
-        </div>
 
-        <!-- UNITUR ma'lumotlari -->
-        <div style="width:70%; padding:10px;">
-            <h2 class="headingText">UNITUR saytiga xush kelibsiz</h2>
-            <p class="paraText">
-                Mavjud sayohatingizni rejalashtiring va buyurtma qiling. Orzularingizdagi ta'tilni yarating. 
-                <br><br>
-                UNITUR sayti orqali siz hayratlanarli tabiiy go'zalliklar va boshqa ko'plab sayohat imkoniyatlarini topasiz.
-                Colorado daryosida oq suvda suzish, mukammal golf maydonlari va Redland cho'lida joylashgan go'zal tabiat — barchasi siz uchun.
-            </p>
-        </div>
-    </div>
+</table>
 
-    <?php include('bottom.php'); ?>
+</div>
+
+<div style="width:800px; float:left">
+<table cellpadding="0px" cellspacing="0" width="1000px">
+<tr><td class="headingText">Welcome to My Tour</td></tr>
+<tr><td class="paraText" width="900px">Plan and Book Your Perfect Trip.Create your dream holiday.
+what you like. Do what you love.
+What's New Explore new experiences, attractions, food and wine trends.
+What will you find during your visit to My Tour? Awe-inspiring natural beauty and the dramatic
+red rock landscape of the Colorado National Monument. Exhilarating outdoor adventures including
+hiking, camping or skiing on the Grand Mesa. Hundreds of miles of world-class mountain biking trails
+such as the Kokopelli Trail. Incredible whitewater rafting on the Colorado River. Stunning golf courses
+whose green fairways are juxtaposed against the craggy Redland desert. Peaceful places to reflect and
+unwind amidst the natural splendor of Colorado's Western Slope. A charming downtown full of great
+shops, restaurants, art galleries and so much more. This is My Tour, where you can experience
+beautiful tourist places.</td><td style="background-image:url(images/13.jpg); background-repeat:no-repeat; color:#FFF; font-family:Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:24px; " width="700px" height="250px" ><div style="background:linear-gradient(#09F,#096,#09F); vertical-align:text-top; padding-left:5%;  width:100%;">HAVE A GOOD TIME &nbsp;&nbsp;&nbsp; without spending a dime</div	></td></tr></table>
+
+</div>
+
+</div>
+
+<div style="clear:both"></div>
+
+<?php include('bottom.php'); ?>
 </body>
 </html>
-
