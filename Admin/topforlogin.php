3ab754c275e6dd4c2e125289ee7b3979e96561fa
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-19 02:20:45 +0500

    Update topforlogin.php

--- Admin/topforlogin.php
+++ Admin/topforlogin.php
@@ -3,7 +3,7 @@
 		  <div class="container">
 			 <div class="top-nav">
 						<div class="logo">
-						<a href="#"><img src="images/logo.png" id="section-1" width="120" height="25" class="img-responsive" alt="UNITUR"/></a>
+						<a href="#"><img src="images/logo.png" id="section-1" width="120" height="25" class="img-responsive" alt="U"/></a>
 						</div>
 						
 			 </div>
